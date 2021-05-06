7c7d2e2bcd0f29c7698fa49501dab2cbae644232
Nethmina senarathne <msenarathne96@gmail.com>
2021-05-06 21:17:30 +0530

    Creating methods for the queue

---
+++ Queue/src/ds1/Queue.java
@@ -0,0 +1,13 @@
+package ds1;
+
+public class Queue {
+
+    public void enqueue(int number){}
+    public void dequeue(){}
+    public boolean empty(){return false;}
+    public int peek(){return 0;}
+    public void clear(){}
+    public void print(){}
+    public boolean contains(int numbers){return false;}
+    public int size(){return 0;}
+}
