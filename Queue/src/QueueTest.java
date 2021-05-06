d775a8d64ecc80b02a66db591d9efba0aef8fcd9
Nethmina senarathne <msenarathne96@gmail.com>
2021-05-06 23:31:31 +0530

    Finalized queue

--- Queue/src/QueueTest.java
+++ Queue/src/QueueTest.java
@@ -6,14 +6,15 @@ public class QueueTest {
         queue.enqueue(10);
         queue.enqueue(20);
         queue.enqueue(30);
-        queue.enqueue(200);
-        queue.enqueue(344);
-        queue.enqueue(45);
         queue.print();
         queue.dequeue();
+        queue.enqueue(50);
         queue.print();
         System.out.println(queue.peek());
         System.out.println(queue.contains(200));
         System.out.println(queue.size());
+        queue.clear();
+        queue.print();
+
     }
 }
