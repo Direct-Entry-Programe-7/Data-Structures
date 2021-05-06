645faf9caf83b08348144f8f7e8f066f84f4d4c5
Nethmina senarathne <msenarathne96@gmail.com>
2021-05-06 18:32:57 +0530

    Create class structure

---
+++ Stack/src/ds/Stack.java
@@ -0,0 +1,32 @@
+package ds;
+
+public class Stack {
+
+    public void push(int number){
+
+    }
+    public void pop(){
+
+    }
+    public  boolean empty(){
+        return false;
+
+    }
+    public int peek(){
+        return  0;
+
+    }
+    public void clear(){
+
+    }
+    public void print(){
+
+    }
+    public boolean contains(){
+        return false;
+    }
+    public int size(){
+        return 0;
+
+    }
+}
