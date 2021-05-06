aac3d43b86b9e96d13978a56429a1b6a950df405
Nethmina senarathne <msenarathne96@gmail.com>
2021-05-06 10:59:35 +0530

    Creating a stack

---
+++ Stack/src/Stack.java
@@ -0,0 +1,10 @@
+public class Stack {
+    public static void main(String[] args) {
+        java.util.Stack numbers =new java.util.Stack();
+        numbers.push(1);
+        numbers.push(2);
+
+
+
+    }
+}
