fc745b8a1d73d1e0f9fcc20045f8d399e9a00067
Nethmina senarathne <msenarathne96@gmail.com>
2021-05-06 11:24:46 +0530

    Adding values to the stack

--- Stack/src/Stack.java
+++ Stack/src/Stack.java
@@ -1,15 +1,13 @@
 public class Stack {
-    public static void main(String[] args) {
-        /*java.util.Stack numbers =new java.util.Stack();
-        numbers.push(10);
-        numbers.push(22);
-        numbers.push(34);
-        numbers.push(45);
+    int size;
+    int array[];
+    int top;
 
-        System.out.println("Values in the Stack: ");
-        System.out.println(numbers);*/
-        int x;
-        int numbers[] = new int[4];
+    public Stack(int size){
+        this.size = size;
+        this.array = new int[size];
+
+
 
 
 
