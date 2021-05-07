f494b27339543e1774f1d25acb2819c3d5eae8bd
Nethmina senarathne <msenarathne96@gmail.com>
2021-05-07 18:28:39 +0530

    Finalize the deque

--- Deque/src/DequeTest.java
+++ Deque/src/DequeTest.java
@@ -1,5 +1,16 @@
+import ds1.Deque;
+
 public class DequeTest {
     public static void main(String[] args) {
+        Deque deque = new Deque();
+        deque.enqueue(10);
+        deque.enqueue(20);
+        deque.print();
+        deque.enqueueRear(200);
+        deque.print();
+        deque.dequeFront();
+        deque.print();
+
 
     }
 }
