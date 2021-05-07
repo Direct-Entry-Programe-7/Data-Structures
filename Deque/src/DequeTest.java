4653f03e7ba7edcec851d85a5c1a02c15124babe
Nethmina senarathne <msenarathne96@gmail.com>
2021-05-07 11:09:00 +0530

    deque 2

--- Deque/src/DequeTest.java
+++ Deque/src/DequeTest.java
@@ -1,11 +1,2 @@
-import ds.Deque;
-
 public class DequeTest {
-    public static void main(String[] args) {
-        Deque deque = new Deque();
-
-        deque.s
-
-
-    }
 }
--- Deque/src/ds/Queue.java
+++ Deque/src/ds/Queue.java
@@ -1,6 +1,4 @@
-package ds1;
-
-import java.util.Arrays;
+package ds;
 
 public class Queue {
 
