0743b7d7f8c9d1d06cd0b663f79816e366f4e39c
Nethmina senarathne <msenarathne96@gmail.com>
2021-05-07 18:37:22 +0530

    Finalize the deque 2

---
+++ Deque2/src/DequeTest.java
@@ -0,0 +1,16 @@
+import ds2.Deque;
+
+public class DequeTest {
+    public static void main(String[] args) {
+        Deque deque = new Deque();
+        deque.enqueue(10);
+        deque.enqueue(20);
+        deque.print();
+        deque.enqueueRear(200);
+        deque.print();
+        deque.dequeFront();
+        deque.print();
+
+
+    }
+}
--- Deque2/src/ds2/Deque.java
+++ Deque2/src/ds2/Deque.java
@@ -4,7 +4,33 @@ import ds1.Queue;
 
 public class Deque extends Queue {
 
+    public void enqueueRear(int number) {
+        if(numbers == null){
+            numbers = new int[1];
+            numbers[0] = number;
+        }else{
+            int[] temp = new int[numbers.length + 1];
+            for (int i = 0; i <numbers.length; i++) {
+                temp[i+1] = numbers[i];
+            }
+            temp[0] = number;
+            numbers = temp;
+        }
+    }
 
+    public void dequeFront(){
+        if(numbers == null){
+            System.err.println("No item to deque from empty array");
+            return;
+        }else  if (numbers.length == 1){
+            numbers = null;
+            return;
+        }
+        int[] temp = new int[numbers.length -1];
+        for (int i = 0; i < temp.length; i++) {
+            temp[i] = numbers[i];
+        }
+        numbers = temp;
 
-
+    }
 }
